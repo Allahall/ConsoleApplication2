032a98356582fa202644d9d07e03684c7f7cc32e
student <student@C246U44>
2022-09-08 15:11:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>      //For I/O Functions
+#include <windows.h>   //For Beep & Sleep functions
+#include<stdlib.h>  //For System Pause
+using namespace std;
+
+int main()
+{
+    int k = 0;
+    for (int i = 0; i < 100000; i++) {
+        k += 10;
+        Beep(k, 100); Sleep(5);
+    }
+
+
+    system("pause");
+    return 0;
+}   //Press any key to exit
